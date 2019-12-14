b1c664802d149bd4021263e10d3274a1ceaf93f9
moviedatascience <ericdavidsmith91@gmail.com>
2019-12-14 18:38:45 -0500

    Continued incrementation of an additional 1k for each numbered scraper file

--- web_scraping/scraper5.py
+++ web_scraping/scraper5.py
@@ -201,7 +201,7 @@ def imdb_scraper(id_list):
   return df
 
 
-id_list = [row for row in df.iloc[22001:29000, 1]]
+id_list = [row for row in df.iloc[106001:121000, 1]]
 df2 = imdb_scraper(id_list)
 # print(df.head())
 
