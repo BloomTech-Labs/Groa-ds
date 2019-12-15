08f03bb5c2f28c8d5489184e8ee4755f6ff06ef4
moviedatascience <ericdavidsmith91@gmail.com>
2019-12-14 21:59:37 -0500

    Adding 4k to all scrapers and creating 5 new scrapers

--- web_scraping/scraper6.py
+++ web_scraping/scraper6.py
@@ -196,7 +196,7 @@ def imdb_scraper(id_list):
   return df
 
 
-id_list = [row for row in df.iloc[27001:31000, 1]]
+id_list = [row for row in df.iloc[75001:79000, 1]]
 df2 = imdb_scraper(id_list)
 # print(df.head())
 
