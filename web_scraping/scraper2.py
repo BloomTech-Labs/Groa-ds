4da4b088d45e8692961e0c94bb329f1d18d54ef7
moviedatascience <ericdavidsmith91@gmail.com>
2019-12-14 21:34:40 -0500

    Incrementing all numbered scraper files +4k and removing review_id/index from scraper5+6

--- web_scraping/scraper2.py
+++ web_scraping/scraper2.py
@@ -196,7 +196,7 @@ def imdb_scraper(id_list):
   return df
 
 
-id_list = [row for row in df.iloc[11001:15000, 1]]
+id_list = [row for row in df.iloc[39001:43000, 1]]
 df2 = imdb_scraper(id_list)
 # print(df.head())
 
--- web_scraping/scraper7.py
+++ web_scraping/scraper7.py
@@ -196,7 +196,7 @@ def imdb_scraper(id_list):
   return df
 
 
-id_list = [row for row in df.iloc[31001:35000, 1]]
+id_list = [row for row in df.iloc[51001:55000, 1]]
 df2 = imdb_scraper(id_list)
 # print(df.head())
 
