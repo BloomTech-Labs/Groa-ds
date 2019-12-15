893b86e2e5fc549511547d3703bd9ac346eced13
moviedatascience <ericdavidsmith91@gmail.com>
2019-12-15 02:27:17 -0500

    Adding 4k to last 6 scrapers needed to get final review data

--- web_scraping/scraper2.py
+++ web_scraping/scraper2.py
@@ -196,7 +196,7 @@ def imdb_scraper(id_list):
   return df
 
 
-id_list = [row for row in df.iloc[203001:207000, 1]]
+id_list = [row for row in df.iloc[251001:255000, 1]]
 df2 = imdb_scraper(id_list)
 # print(df.head())
 
--- web_scraping/scraper4.py
+++ web_scraping/scraper4.py
@@ -196,7 +196,7 @@ def imdb_scraper(id_list):
   return df
 
 
-id_list = [row for row in df.iloc[211001:215000, 1]]
+id_list = [row for row in df.iloc[259001:263000, 1]]
 df2 = imdb_scraper(id_list)
 # print(df.head())
 
