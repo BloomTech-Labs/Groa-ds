f0c16c73c149debc56b90ceeaf51d31cfd79a6a0
cmgospod <cmgospod@users.noreply.github.com>
2019-12-20 18:49:31 -0600

    Scraper now correctly inserts reviews in batches. Added functioning error handling for 404 errors. Additional minor code cleanup. Removed testing query and print statements.

--- web_scraping/scraper.py
+++ web_scraping/scraper.py
@@ -16,20 +16,26 @@ df = pd.read_csv('movieid_shuffle.csv', encoding='ascii')
 
 # connect to database
 connection = psycopg2.connect(
-    database  = "postgres",
-    user      = "postgres",
-    password  = getpass(),
-    host      = "movie-rec-scrape.cvslmiksgnix.us-east-1.rds.amazonaws.com",
-    port      = '5432'
-)
+    database = "postgres",
+    user     = "postgres",
+    password = getpass(),
+    host     = "movie-rec-scrape.cvslmiksgnix.us-east-1.rds.amazonaws.com",
+    port     = "5432"
+    )
 
 # cursor object
 cursor_boi = connection.cursor()
 
 print("Connected!")
 
-def create_log(movie_name,num_review,num_nan,elapsed):
-    # path = "C:\\Documents\\Atom\\Labs 2019\\Movie recommender\\web scraping\\logs"
+
+def create_log(movie_name, num_review, num_nan, elapsed):
+    """
+    Creates a log for each movie scraped.
+
+    Takes info generated within imdb_scraper to produce a log file to return
+    a log of the movie name, the number of reviews, and the time taken
+    """
     path = os.getcwd()
     os.chdir(path)
 
@@ -49,10 +55,16 @@ def create_log(movie_name,num_review,num_nan,elapsed):
 
 
 def imdb_scraper(id_list):
+    """
+    Scrapes imbd.com for review pages.
+
+    create_log, make_dataframe, and insert_rows are intended to be used inside
+    this function. Takes in the id of the movie in "ttxxxxxxx" format, robust
+    to different numbers of numerals. Fails gracefully on movies with no
+    reviews, moving on without returning anything.
+    """
     t = time.perf_counter()
     # DataFrame Columns (not in order)
-    index = []
-    index_ = 0
     movie_id = []
     rating = []
     reviews = []
@@ -68,42 +80,45 @@ def imdb_scraper(id_list):
         Nan_count = 0
         review_count = 0
         movie_title = ''
-        review_count = 0
 
 
         url_short = f'http://www.imdb.com/title/{id}/'
         url_reviews = url_short + 'reviews?ref_=tt_urv'
         time.sleep(randint(3,6))
         response = requests.get(url_reviews)
+        if response.status_code != 200:
+            continue
         soup = BeautifulSoup(response.text, 'html.parser')
         items = soup.find_all(class_='lister-item-content')
-        try:
-            movie_title = (soup.find(class_ = "subnav_heading").get_text())
-        except:
-            pass
+
 
         while True:
             if iteration_counter > 9999:
-                make_dataframe(index, movie_id, review, rating, title, username,
+                make_dataframe(movie_id, review, rating, title, username,
                                found_useful_num, found_useful_den, date)
                 insert_rows(df)
-
+                movie_id = []
+                rating = []
+                reviews = []
+                titles = []
+                username = []
+                found_useful_num = []
+                found_useful_den = []
+                date = []
                 iteration_counter = 0
             # populate lists
             for item in items:
                 review_count += 1
-                reviews.append(item.find(class_ = "text show-more__control").get_text())
-                titles.append(item.find(class_ = "title").get_text())
-                username.append(item.find(class_ = "display-name-link").get_text())
-                date.append(item.find(class_ = "review-date").get_text())
+                reviews.append(item.find(class_="text show-more__control").get_text())
+                titles.append(item.find(class_="title").get_text())
+                username.append(item.find(class_="display-name-link").get_text())
+                date.append(item.find(class_="review-date").get_text())
                 movie_id.append(int(id.replace("tt", "")))
-                found_useful = item.find(class_ = "actions text-muted").get_text()
+                found_useful = item.find(class_="actions text-muted").get_text()
                 found_useful = found_useful.replace(",", "")
                 usefuls = [int(i) for i in found_useful.split() if i.isdigit()]
                 found_useful_num.append(usefuls[0])
                 found_useful_den.append(usefuls[1])
-                index.append(index_)
-                index_ += 1
                 try:
                     rating.append(item.find(class_="rating-other-user-rating").find('span').text)
                 except:
@@ -133,16 +148,16 @@ def imdb_scraper(id_list):
         # for loop ends here
 
     # create DataFrame
-    df = make_dataframe(index, movie_id, review, rating, title, username,
+    df = make_dataframe(movie_id, review, rating, title, username,
                         found_useful_num, found_useful_den, date)
     df = insert_rows(df)
-    return df
+    print('All done!')
 
 
-def make_dataframe(index, movie_id, review, rating, title, username, found_useful_num, found_useful_den, date):
+def make_dataframe(movie_id, review, rating, title, username,
+                   found_useful_num, found_useful_den, date):
     df = pd.DataFrame(
            {
-            'index':index,
             'movie_id':movie_id,
             'review':reviews,
             'rating':rating,
@@ -160,46 +175,38 @@ def insert_rows(df):
     # convert rows into tuples
     row_insertions = ""
     for i in list(df.itertuples(index=False)):
-        row_insertions += str((i.index,
-                                str(i.username.replace("'", "").replace('"', '')),
-                                i.movie_id,
-                                i.date,
-                                str(i.review.replace("'", "").replace('"', '')),
-                                str(i.title.replace("'", "").replace('"', '')),
-                                int(i.rating),
-                                i.helpful_num,
-                                i.helpful_denom)) + ", "
+        row_insertions += str((str(i.username.replace("'","").replace('"','')),
+                               i.movie_id,
+                               i.date,
+                               str(i.review.replace("'","").replace('"','')),
+                               str(i.title.replace("'","").replace('"','')),
+                               int(i.rating),
+                               i.helpful_num,
+                               i.helpful_denom)) + ", "
 
     # remove hanging comma
     row_insertions = row_insertions[:-2]
 
     # create SQL INSERT query
-    query = """INSERT INTO reviews(review_id,
-                                    username,
-                                    movie_id,
-                                    review_date,
-                                    review_text,
-                                    review_title,
-                                    user_rating,
-                                    helpful_num,
-                                    helpful_denom)
-                                    VALUES """ + row_insertions + ";"
-    # simple test query
-    test_query = """INSERT INTO reviews(review_id, username, movie_id, review_date, review_text, review_title, user_rating, helpful_num, helpful_denom) VALUES (1, 'coop', 12345678, '2016-06-23', 'I love this movie!', 'Me happy', 5, 6, 12 )"""
+    query = """INSERT INTO reviews(username,
+                                   movie_id,
+                                   review_date,
+                                   review_text,
+                                   review_title,
+                                   user_rating,
+                                   helpful_num,
+                                   helpful_denom)
+                                   VALUES """ + row_insertions + ";"
 
     # execute query
     cursor_boi.execute(query)
     connection.commit()
-    print(query)
+    print("Insertion Complete")
 
     # total time it took to scrape each review
     t3 = time.perf_counter()
     total = t3 - t
     print(f"Scraped {len(id_list)} movies in {round(total,2)} seconds")
-    return df
-
-id_list = [row for row in df.iloc[:1000, 1]]
-df2 = imdb_scraper(id_list)
 
 # close connection
 if connection:
