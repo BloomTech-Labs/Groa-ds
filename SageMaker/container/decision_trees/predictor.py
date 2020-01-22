24c10a1cbe34079e4d013bdea9c043710ebb7336
me <me>
2020-01-22 08:25:02 +0000

    "updating sagemaker container"

--- SageMaker/container/decision_trees/predictor.py
+++ SageMaker/container/decision_trees/predictor.py
@@ -6,10 +6,11 @@ from __future__ import print_function
 import os
 import json
 import gensim
+from gensim.models.word2vec import Word2Vec
 import numpy as np
 import pandas as pd
 import pickle
-import StringIO
+from io import StringIO
 import sys
 import signal
 import traceback
@@ -33,45 +34,47 @@ class ScoringService(object):
         """Get the model object for this instance, loading it if it's not already loaded."""
         if cls.model == None:
             # load the gensim model
-            with open(os.path.join(model_path, 'word2vec_2.model'), 'r') as inp:
-                gensim.models.KeyedVectors.load_word2vec_format(inp)
-            w2v_model = gensim.models.KeyedVectors.load_word2vec_format("word2vec_2.model")
-            # keep only the normalized vectors.
-            # This saves memory but makes the model untrainable (read-only).
-            w2v_model = w2v_model.init_sims(replace=True)
+            w2v_model = Word2Vec.load(os.path.join(model_path, 'word2vec_2.model'))
+            print(w2v_model.vector_size)
+            #w2v_model = w2v_model.init_sims(replace=True)
             cls.model = w2v_model
         return cls.model
 
     @classmethod
     def predict(cls, input):
         """For the input, do the predictions and return them.
-
         Args:
             input (a pandas dataframe): The data on which to do the predictions. There will be
                 one prediction per row in the dataframe"""
-        # get the model
+
         clf = cls.get_model()
+        print(clf)
+        
 
-        ## convert a csv input
-        # input = pd.read_csv(input)
-
+        def _similar_movies(v, n = 6):
+            # extract most similar movies for the input vector
+            #v = v.reshape(-1)
+            ms = clf.similar_by_vector(v, topn= n+1)#[1:]
+            
+            return ms
+        
         def _aggregate_vectors(movies):
             # get the vector average of the movies in the input
             movie_vec = []
-            for i in movies:
+            for i in movies.values:
                 try:
                     movie_vec.append(clf[i])
                 except KeyError:
                     continue
-
+            
             return np.mean(movie_vec, axis=0)
 
-        def _similar_movies(v, n = 6):
-            # extract most similar movies for the input vector
-            return clf.similar_by_vector(v, topn= n+1)[1:]
-
-        new_input = [[x[0].lstrip("0")] for x in input[input.columns[0]]] # remove leading zeroes
-        recs = _similar_movies(_aggregate_vectors(new_input))
+        #input = [[str(x) for x in lst] for lst in input] # remove leading zeroes
+        #print(input)
+        print("Printing I guess")
+        new = _aggregate_vectors(input)
+        #print(new)
+        recs = _similar_movies(new)
         return recs
 
 # The flask app for serving predictions
@@ -100,8 +103,9 @@ def transformation():
     # Convert from CSV to pandas
     if flask.request.content_type == 'text/csv':
         data = flask.request.data.decode('utf-8')
-        s = StringIO.StringIO(data)
+        s = StringIO(data)
         data = pd.read_csv(s, header=None)
+        print(data)
     else:
         return flask.Response(response='This predictor only supports CSV data', status=415, mimetype='text/plain')
 
@@ -111,7 +115,7 @@ def transformation():
     predictions = ScoringService.predict(data)
 
     # Convert from numpy back to CSV
-    out = StringIO.StringIO()
+    out = StringIO()
     pd.DataFrame({'results':predictions}).to_csv(out, header=False, index=False)
     result = out.getvalue()
 
