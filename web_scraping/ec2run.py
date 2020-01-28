51cd58efe9b5c7f417fd902a183072c986e3e0d1
cmgospod <cmgospod@users.noreply.github.com>
2020-01-28 12:34:12 -0600

    Added dummy ec2run file

---
+++ web_scraping/ec2run.py
@@ -0,0 +1,13 @@
+from scraper.py import *
+
+
+
+
+
+start =
+end =
+max_iter =
+scraper_instance =
+s = Scraper(start=start, end=end, max_iter=max_iter, scraper_instance=scraper_instance)
+s.get_ids()
+s.scrape_letterboxd()
