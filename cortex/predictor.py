f7fa7e0c6ec35a92681f00a0694da0150799368d
rileythejones <rileymjones@gmail.com>
2020-03-02 16:14:11 -0700

    IMDB data handling

--- cortex/predictor.py
+++ cortex/predictor.py
@@ -13,75 +13,69 @@ warnings.filterwarnings('ignore')
 
 class PythonPredictor:
     def __init__(self, config={}):
-        """ Requires configuration from cortex.yaml """
-         
-        # When using s3 bucket to download the model
-        # s3 = boto3.client("s3")
-        # s3.download_file(config["bucket"], config["key"], "w2v_limitingfactor_v3.51.model")
 
         self.model = Recommender('models/w2v_limitingfactor_v3.51.model')
 
         pass
 
     def predict(self, payload): # recieves userid, outputs recommendation_id
-        """Called once per request. Runs preprocessing of the request payload, inference, and postprocessing of the inference output. Required.
-
-        Args:
-            payload: The parsed JSON request payload.
-
-        Returns:
-            Prediction or a batch of predictions.
-        """
 
         self.model.connect_db()
         user_id = payload
         
-        """ testing with local letterboxd data """
-        
-#         ratings = pd.read_csv('exported_data/imdb/riley_imdb_ratings.csv', engine='python')
-#         ratings = pd.read_csv('exported_data/letterboxd/riley/ratings.csv',  engine='python')
-        
+        """ Check if user id exists """
         query = "SELECT EXISTS(SELECT 1 FROM user_letterboxd_ratings where user_id=%s);" 
         self.model.cursor_dog.execute(query, (user_id,))
-        boolean = self.model.cursor_dog.fetchall()
+        boolean_letterboxd = self.model.cursor_dog.fetchall()
+        if boolean_letterboxd[0][0]==False: 
+            print("letterboxd user_id not found")
         
-        if boolean[0][0]==False: # True
+        query = "SELECT EXISTS(SELECT 1 FROM user_imdb_ratings where user_id=%s);" 
+        self.model.cursor_dog.execute(query, (user_id,))
+        boolean_imdb = self.model.cursor_dog.fetchall()
+        if boolean_imdb[0][0]==False: 
+            print("imdb user_id not found")
+            
+        if ((boolean_letterboxd[0][0]==False) & (boolean_imdb[0][0]==False)):
             self.model.cursor_dog.close()
             self.model.connection.close()
-            return "user_id not found"
+            return "Neither IMDB or Letterboxd user_id found"
+        
+        """ IMDB data """
+        if boolean_imdb[0][0]:
+            self.model.cursor_dog.execute("SELECT date, name, year, rating FROM user_imdb_ratings WHERE user_id=%s;", (user_id,))
+            ratings_sql= self.model.cursor_dog.fetchall()
+            im = pd.DataFrame(ratings_sql, columns = ['Date', 'Name', 'Year', 'Letterboxd URI', 'Rating'])
+            drop = ['Your Rating', 'Date Rated', 'Title', 'Const', 'URL', 'Title Type', 'IMDb Rating', 'Runtime (mins)', 'Genres', 'Num Votes', 'Release Date', 'Directors']
+            im['Rating'] = im['Your Rating']
+            im['Date'] = im['Date Rated']
+            im['Name'] = im['Title']
+            im = im.drop(columns=drop)
     
-    
+        """ Letterboxd data """
         self.model.cursor_dog.execute("SELECT date, name, year, letterboxd_uri, rating FROM user_letterboxd_ratings WHERE user_id=%s;", (user_id,))
         ratings_sql= self.model.cursor_dog.fetchall()
         ratings = pd.DataFrame(ratings_sql, columns = ['Date', 'Name', 'Year', 'Letterboxd URI', 'Rating'])
         ratings= ratings.dropna()
         
-
         self.model.cursor_dog.execute("SELECT date, name, year, letterboxd_uri FROM user_letterboxd_watchlist WHERE user_id=%s;", (user_id,))
         watchlist_sql= self.model.cursor_dog.fetchall()
         watchlist = pd.DataFrame(watchlist_sql, columns = ['Date', 'Name', 'Year', 'Letterboxd URI'])
         watchlist = watchlist.dropna()
         
-
         self.model.cursor_dog.execute("SELECT date, name, year, letterboxd_uri FROM user_letterboxd_watched WHERE user_id=%s;", (user_id,))
         watched_sql= self.model.cursor_dog.fetchall()
         watched = pd.DataFrame(watched_sql, columns = ['Date', 'Name', 'Year', 'Letterboxd URI'])
         watched = watched.dropna()
         
-
-#         self.model.cursor_dog.execute("SELECT * FROM test_title_basics_small;")
-#         title_basics_small_sql= self.model.cursor_dog.fetchall()
-#         id_book = pd.DataFrame(title_basics_small_sql, columns = ['tconst', 'primaryTitle', 'originalTitle', 'startYear'])
-#         id_book = id_book.dropna()
-        
         """ Prepare data  """
+        
+        if boolean_imdb[0][0]:
+            if len(im) > len(ratings):
+                ratings = im
+        
         good_list, bad_list, hist_list, val_list, ratings_dict = prep_data(
                                     ratings, watched_df=None, watchlist_df=None, good_threshold=3, bad_threshold=2) 
-        
-        """ Load JSON into a list (if applicable) """ 
-        # payload_jsonified = json.dumps(payload)
-        # movie_dict = json.loads(payload_jsonified)
-        # movie_list = list(movie_dict.values())
         
         """ Run prediction with parameters """
         
